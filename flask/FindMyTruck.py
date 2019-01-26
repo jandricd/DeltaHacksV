55638bf2f1954586d65f011208cfe44b230cda53
jandricd <jandricd@mcmaster.ca>
2019-01-26 18:45:39 -0500

    -simple form

--- flask/FindMyTruck.py
+++ flask/FindMyTruck.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+@app.route('/gpsdata')
+def query():
+    return
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        beginlat = request.form.get('beginlat')
+        beginlong = request.form.get('beginlong')
+        endlat = request.form.get('endlat')
+        endlong = request.form.get('endlong')
+        weight = request.form.get('weight')
+        return '''<h1>[{},{},{},{},{}]</h1>'''.format(beginlat,beginlong,endlat,endlong,weight)
+    
+    return '''<form method="POST">
+                beginlat: <input type="text" name="beginlat"><br>
+                beginlong: <input type="text" name="beginlong"><br>
+                endlat: <input type="text" name="endlat"><br>
+                endlong: <input type="text" name="endlong"><br>
+                weight: <input type="text" name="weight"><br>  
+                <input type="submit" value="Submit"><br>
+              </form>'''
 
 @app.route('/')
 def display():
-    return "it verkskskksksksksssss"
+    return "hello"
 
 if __name__ == '__main__':
     app.run(host='172.17.99.57', debug=True, port=3134)
